76e197195e76ece77b7405217f6c964a371c8744
tutu <227384408@qq.com>
2022-11-14 20:10:10 +0800

    finish home

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,19 +1,162 @@
 <template>
   <view>
-    
+    <!-- 搜索组件 -->
+    <view class="search-box">
+      <my-search @myclick='gotoSearch'></my-search>
+    </view>
+    <!-- 轮播图 -->
+    <swiper :indicator-dots="true" :autoplay="true" :interval="3000" :circular="true">
+      <swiper-item v-for="(item,i) in swiperList" :key="i">
+        <navigator class="swiper-item" :url="'/subpkg/goods_detail/goods_detail?goods_id='+item.goods_id">
+          <image :src="item.image_src"></image>
+        </navigator>
+      </swiper-item>
+    </swiper>
+    <!-- 分类导航 -->
+    <view class="nav-list">
+      <view class="nav-item" v-for="(item,i) in navList" :key="i" @click="navClickHander(item)">
+        <image :src="item.image_src" class="nav-img"></image>
+      </view>
+    </view>
+    <!-- 楼层区域 -->
+    <view class="floor-list">
+      <view class="floor-item" v-for="(item , i) in floorList">
+        <image :src="item.floor_title.image_src" class="floor-title"></image>
+
+        <view class="floor-img-box">
+          <!-- 左侧大盒子 -->
+          <navigator class="left-img-box" :url="item.product_list[0].url">
+            <image :src="item.product_list[0].image_src" :style="{width:item.product_list[0].image_width+'rpx'}">
+            </image>
+          </navigator>
+          <!-- 右侧4个小盒子 -->
+          <view class="right-img-box">
+            <navigator class="right-img-item" v-for="(item2,i2) in item.product_list.slice(1)" :key="i2"
+              :url="item2.url">
+              <image :src="item2.image_src" mode="widthFix" :style="{width:item2.image_width+'rpx'}"></image>
+            </navigator>
+          </view>
+        </view>
+      </view>
+    </view>
   </view>
 </template>
 
 <script>
+  import {
+    $http
+  } from '@escook/request-miniprogram';
   export default {
     data() {
       return {
-        
+        //轮播图数据列表
+        swiperList: [],
+        //分类数据列表
+        navList: [],
+        //楼层数据列表
+        floorList: []
       };
+    },
+    onLoad() {
+      this.getSwiperList()
+      this.getNavList()
+      this.getFloorList()
+    },
+    methods: {
+      // 获取数据函数
+      async getSwiperList() {
+        const {
+          data: res
+        } = await $http.get('/api/public/v1/home/swiperdata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.swiperList = res.message
+      },
+      async getNavList() {
+        // console.log('this.navList')
+        const {
+          data: res
+        } = await $http.get('/api/public/v1/home/catitems')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        this.navList = res.message
+        // uni.showToast({
+        //   title: '分类请求成功',
+        //   duration: 1500
+        // })
+      },
+      async getFloorList() {
+        const {
+          data: res
+        } = await $http.get('/api/public/v1/home/floordata')
+        if (res.meta.status !== 200) return uni.$showMsg()
+        res.message.forEach(floor => {
+          floor.product_list.forEach(prod => {
+            prod.url = '/subpkg/goods_list/goods_list?' + prod.navigator_url.split('?')[1]
+          })
+        })
+        this.floorList = res.message
+      },
+      //分类点击事件
+      navClickHander(item) {
+        if (item.name === '分类')
+          uni.switchTab({
+            url: '/pages/cate/cate'
+          })
+      },
+      gotoSearch() {
+        uni.navigateTo({
+          url: '/subpkg/search/search'
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
+  //搜索吸顶效果
+  .search-box {
+    position: sticky;
+    top: 0;
+    z-index: 999;
+  }
 
+  //轮播图
+  swiper {
+    height: 330rpx;
+
+    .swiper-item,
+    image {
+      width: 100%;
+      height: 100%;
+    }
+  }
+
+  //分类
+  .nav-list {
+    display: flex;
+    justify-content: space-around;
+    margin: 15px 0;
+
+    .nav-img {
+      width: 128rpx;
+      height: 140rpx;
+    }
+  }
+
+  // 楼层
+  .floor-title {
+    height: 60rpx;
+    width: 100%;
+    display: flex;
+  }
+
+  .floor-img-box {
+    display: flex;
+    padding-left: 10rpx;
+
+    .right-img-box {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-around;
+    }
+  }
 </style>
